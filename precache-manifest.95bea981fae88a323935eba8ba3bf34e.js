670d3c7fb2952ccf3306760bf146dacd0bea25c0
Neetu Chauhan <neetuchauhan0421@gmail.com>
2020-07-06 11:57:49 +0530

    Updates

---
+++ precache-manifest.95bea981fae88a323935eba8ba3bf34e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f4d33ee55c1331506fee87baeb8076",
+    "url": "/budgetApp/index.html"
+  },
+  {
+    "revision": "70c39d30346493206b44",
+    "url": "/budgetApp/static/css/main.d1552f98.chunk.css"
+  },
+  {
+    "revision": "6d6519013cf7d5f85d1d",
+    "url": "/budgetApp/static/js/2.a90ca4a7.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/budgetApp/static/js/2.a90ca4a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c39d30346493206b44",
+    "url": "/budgetApp/static/js/main.e65c8e23.chunk.js"
+  },
+  {
+    "revision": "9e43a85c5f6cbb8ed4c2",
+    "url": "/budgetApp/static/js/runtime-main.ff87e5ca.js"
+  }
+]);
